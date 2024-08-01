0f8d1d079a6f59e7af3c3050dab4ee6adc084cf3
sonalipatel223 <sonalipatel07896@gmail.com>
2024-08-01 17:58:21 +0530

    ok

---
+++ projectORS/src/main/java/com/rays/common/ApplicationExceptionHandlerCtl.java
@@ -0,0 +1,25 @@
+
+package com.rays.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Handles application propagated exceptions
+ * 
+ * Sonali Patel 
+ *
+ */
+
+
+@ControllerAdvice // handle exception globally
+public class ApplicationExceptionHandlerCtl {
+
+	@ExceptionHandler(value = RuntimeException.class) // custom exception handle
+	public ORSResponse handleRuntimeException(RuntimeException e) {
+		ORSResponse res = new ORSResponse(false);
+		res.addMessage(e.getMessage() + "Sonali Patel");
+		return res;
+	}
+
+}
